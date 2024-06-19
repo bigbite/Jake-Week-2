bc2965f6b654b1d34b710897bf669386950a6d28
Jake Atkinson <jakeatkinson@Jakes-MacBook-Pro.local>
2024-06-19 20:02:49 +0100

    feature+fix/added dropdown menus and removed logos until proper fix is avaliable

--- JS/app.js
+++ JS/app.js
@@ -4,6 +4,10 @@ const dropDownContentSamaritans = document.querySelector('#dropdown-content-sama
 const dropDownContentMind = document.querySelector('#dropdown-content-mind');
 const dropDownContentShout = document.querySelector('#dropdown-content-shout');
 
+/*
+changed the functionality of the javascript to incorporate a drop down arrow easier
+but also to clean up the css classes slightly
+
 dropDownBtns.forEach(function (dropDownBtn) {
   dropDownBtn.addEventListener('click', function () {
     if (dropDownBtn.classList.contains('dropdown-btn-samaritans')) {
@@ -12,6 +16,36 @@ dropDownBtns.forEach(function (dropDownBtn) {
       dropDownContentMind.classList.toggle('dropdown-content-hide');
     } else if (dropDownBtn.classList.contains('dropdown-btn-shout')) {
       dropDownContentShout.classList.toggle('dropdown-content-hide');
+    }
+  });
+});
+*/
+dropDownBtns.forEach(function (dropDownBtn) {
+  dropDownBtn.addEventListener('click', function () {
+    if (dropDownBtn.classList.contains('dropdown-btn-samaritans')) {
+      if (dropDownContentSamaritans.classList.contains('dropdown-content-show')) {
+        dropDownContentSamaritans.classList.add('dropdown-content-hide');
+        dropDownContentSamaritans.classList.remove('dropdown-content-show');
+      } else {
+        dropDownContentSamaritans.classList.remove('dropdown-content-hide');
+        dropDownContentSamaritans.classList.add('dropdown-content-show');
+      }
+    } else if (dropDownBtn.classList.contains('dropdown-btn-mind')) {
+      if (dropDownContentMind.classList.contains('dropdown-content-show')) {
+        dropDownContentMind.classList.add('dropdown-content-hide');
+        dropDownContentMind.classList.remove('dropdown-content-show');
+      } else {
+        dropDownContentMind.classList.remove('dropdown-content-hide');
+        dropDownContentMind.classList.add('dropdown-content-show');
+      }
+    } else if (dropDownBtn.classList.contains('dropdown-btn-shout')) {
+      if (dropDownContentShout.classList.contains('dropdown-content-show')) {
+        dropDownContentShout.classList.add('dropdown-content-hide');
+        dropDownContentShout.classList.remove('dropdown-content-show');
+      } else {
+        dropDownContentShout.classList.remove('dropdown-content-hide');
+        dropDownContentShout.classList.add('dropdown-content-show');
+      }
     }
   });
 });
