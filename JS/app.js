74a4dcaa38233577c55516891e891a2d42e10baa
Jake Atkinson <jakeatkinson@Jakes-MBP.localdomain>
2024-06-25 08:51:31 +0100

    fix/attempting to pull back to feat/contact-page

--- JS/app.js
+++ JS/app.js
@@ -19,32 +19,24 @@ dropDownBtns.forEach(function (dropDownBtn) {
   });
 });
 */
+function handleDropdownClick(dropDownContent) {
+  if (dropDownContent.classList.contains('dropdown-content-show')) {
+    dropDownContent.classList.add('dropdown-content-hide');
+    dropDownContent.classList.remove('dropdown-content-show');
+  } else {
+    dropDownContent.classList.remove('dropdown-content-hide');
+    dropDownContent.classList.add('dropdown-content-show');
+  }
+}
+
 dropDownBtns.forEach(function (dropDownBtn) {
   dropDownBtn.addEventListener('click', function () {
     if (dropDownBtn.classList.contains('dropdown-btn-samaritans')) {
-      if (dropDownContentSamaritans.classList.contains('dropdown-content-show')) {
-        dropDownContentSamaritans.classList.add('dropdown-content-hide');
-        dropDownContentSamaritans.classList.remove('dropdown-content-show');
-      } else {
-        dropDownContentSamaritans.classList.remove('dropdown-content-hide');
-        dropDownContentSamaritans.classList.add('dropdown-content-show');
-      }
+      handleDropdownClick(dropDownContentSamaritans);
     } else if (dropDownBtn.classList.contains('dropdown-btn-mind')) {
-      if (dropDownContentMind.classList.contains('dropdown-content-show')) {
-        dropDownContentMind.classList.add('dropdown-content-hide');
-        dropDownContentMind.classList.remove('dropdown-content-show');
-      } else {
-        dropDownContentMind.classList.remove('dropdown-content-hide');
-        dropDownContentMind.classList.add('dropdown-content-show');
-      }
+      handleDropdownClick(dropDownContentMind);
     } else if (dropDownBtn.classList.contains('dropdown-btn-shout')) {
-      if (dropDownContentShout.classList.contains('dropdown-content-show')) {
-        dropDownContentShout.classList.add('dropdown-content-hide');
-        dropDownContentShout.classList.remove('dropdown-content-show');
-      } else {
-        dropDownContentShout.classList.remove('dropdown-content-hide');
-        dropDownContentShout.classList.add('dropdown-content-show');
-      }
+      handleDropdownClick(dropDownContentShout);
     }
   });
 });
